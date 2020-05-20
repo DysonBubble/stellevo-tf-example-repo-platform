f1551eb83fc19fabdfc3cee5474ddcabe43030cd
Stanislav Muhametsin <346799+stazz@users.noreply.github.com>
2020-05-20 23:57:25 +0300

    Adjusting to changes done in general Stellevo API.

--- api/src/dummy.ts
+++ api/src/dummy.ts
@@ -1,3 +1,20 @@
-import * as common from "../../api/common/platforms";
+import * as common from "../../api/common/platforms/util";
 
-export const DummyApp = () => common.specializeConfig( { configuration: { azuread_application: { "test-app": { "name": "testing" } } } } );
+export const DummyApp = () => common.specializeConfig( {
+  configuration: {
+    resources: {
+      azuread_application: {
+        "test-app": {
+          "name": "testing"
+        }
+      }
+    },
+    data_sources: {
+      azuread_client_config: {
+        "cur-config": {
+
+        }
+      }
+    }
+  }
+} );
